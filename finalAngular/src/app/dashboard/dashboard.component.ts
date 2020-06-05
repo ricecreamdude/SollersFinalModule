36284b82a7c5a140f15783291de290f4ee485143
Joshua Ho <ho.joshua4@gmail.com>
2020-06-05 00:35:26 -0700

    Completed POST method

--- finalAngular/src/app/dashboard/dashboard.component.ts
+++ finalAngular/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Observable, Subscription } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CustomerService } from '../customer.service';
+
 
 
 @Component({
@@ -10,14 +11,26 @@ import { CustomerService } from '../customer.service';
 })
 export class DashboardComponent implements OnInit {
 
+  //Add Customer Form Properties
+  createCustomerForm: FormGroup;
+  subimitted:boolean = false;
+
+  //Customer Display Properties
   employees = [];
   columns = ["First Name", "Last Name", "Email", "Actions" ]
 
-  constructor( private custService: CustomerService ) { }
+  constructor( private custService: CustomerService, private formBuilder:FormBuilder ) { }
 
   ngOnInit(): void {
     //Load our application
     this.getEmployees();
+  
+    this.createCustomerForm = this.formBuilder.group({
+      firstName:  ['', Validators.required],
+      lastName:   ['', Validators.required],
+      email:      ['', [Validators.email, Validators.required] ],
+    })
+  
   }
 
   getEmployees()
@@ -27,13 +40,27 @@ export class DashboardComponent implements OnInit {
     })
   }
 
+  //Todo - Make it so new people are only added to the end of the list
+  createCustomer():void{
+    this.subimitted = true;
+
+    if (this.createCustomerForm.invalid) return;
+
+    let data = this.createCustomerForm.value;
+
+    this.custService.Post( data ).subscribe( res =>{
+      this.getEmployees();
+    })
+
+
+  }
+
   update( emp ):void {
 
     this.custService.Update( emp.id, emp ).subscribe(res => {
       console.log(emp);
     })
-
-
+    
   }
 
   delete( emp ):void {
